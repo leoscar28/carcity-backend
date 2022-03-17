9ae480614cd4c790da98983e1e0502099188df2f
era <admin@smartproperty.kz>
2022-03-17 11:06:06 +0600

    fixed completion list model

--- app/Models/Completion.php
+++ app/Models/Completion.php
@@ -5,9 +5,14 @@ namespace App\Models;
 use App\Domain\Contracts\CompletionContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Completion extends Model
 {
     use HasFactory;
     protected $fillable =   CompletionContract::FILLABLE;
+    public function completionList(): HasMany
+    {
+        return $this->hasMany(CompletionList::class);
+    }
 }
