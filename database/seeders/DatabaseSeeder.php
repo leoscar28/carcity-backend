89a94a4109d27ea2bb277adec2cdaa2bd18a1584
era <admin@smartproperty.kz>
2022-03-25 21:54:12 +0600

    reset carcity db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
         //User::factory()->create();
 
         Application::factory(1000)->create();
-        Invoice::factory(1000)->create();
-        Completion::factory(1000)->create();
+        //Invoice::factory(1000)->create();
+        //Completion::factory(1000)->create();
         $this->call(RoleSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(CompletionStatusSeeder::class);
