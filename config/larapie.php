6863f113be2b6945ea9211f97c3f9a86fbe1f2b2
era <admin@smartproperty.kz>
2022-04-01 15:42:33 +0600

    throttle increased

---
+++ config/larapie.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'parser' => [
+        'route-prefixes' => [
+            'api',
+        ],
+    ],
+
+    'routes' => [
+        'prefix'     => null,
+        'middleware' => ['web'],
+    ],
+
+    'ignore' => [
+        'controllers' => [
+            //
+        ],
+
+        'route-names' => [
+            //
+        ],
+    ],
+];
