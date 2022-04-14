8fc8701bbd2ec8fd2cbae810f00566938f9de6df
era <admin@smartproperty.kz>
2022-04-14 13:02:59 +0600

    changed application sign to 30

--- app/Domain/Repositories/Application/ApplicationRepositoryEloquent.php
+++ app/Domain/Repositories/Application/ApplicationRepositoryEloquent.php
@@ -78,7 +78,7 @@ class ApplicationRepositoryEloquent implements ApplicationRepositoryInterface
             [MainContract::RID,$rid],
             [MainContract::UPLOAD_STATUS_ID,$uploadStatusId],
             [MainContract::STATUS,1]
-        ])->limit(50)->get();
+        ])->limit(30)->get();
     }
 
     public function getByRid($rid)
