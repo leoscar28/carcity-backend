1543ad659feb7e932503a8764d521a45120da958
era <admin@smartproperty.kz>
2022-05-06 16:04:03 +0600

    fixed completion Signature

--- app/Jobs/CompletionSignatureArchive.php
+++ app/Jobs/CompletionSignatureArchive.php
@@ -81,7 +81,7 @@ class CompletionSignatureArchive implements ShouldQueue
                     $fpdi->useTemplate($template);
                     $fpdi->SetFont("helvetica", 'B', 8);
                     $fpdi->SetTextColor(0,0,0);
-                    $top    =   (ceil($signatures[0][0][5]) + 3.2);
+                    $top    =   (floor($signatures[0][0][5]) + 3.2);
                     $left   =   215;
                     $name   =   explode(' ',trim($user2['cert']['chain'][0]['subject']['commonName']));
                     $fpdi->Text($left,$top, substr($match[1], 1, 10));
