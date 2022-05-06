22a91117296c8e4b87e0ec66cdf86e3ca2bb0ddb
era <admin@smartproperty.kz>
2022-05-06 17:45:53 +0600

    fixed completion signature

--- app/Jobs/CompletionSignatureArchive.php
+++ app/Jobs/CompletionSignatureArchive.php
@@ -65,8 +65,9 @@ class CompletionSignatureArchive implements ShouldQueue
                 preg_match('~<ds:SignatureValue>([^{]*)</ds:SignatureValue>~i', $this->xml, $match);
                 if (array_key_exists(1,$match) && $match[1]) {
                     $file   =   storage_path('docs/').$completion->{MainContract::CUSTOMER_ID}.'/completions/'.$completion->{MainContract::ID}.'/'.$completion->{MainContract::ID}.'-signed.pdf';
+                    $file1  =   storage_path('docs/').$completion->{MainContract::CUSTOMER_ID}.'/completions/'.$completion->{MainContract::ID}.'/'.$completion->{MainContract::ID}.'.pdf';
                     $parser = new Parser();
-                    $pdf = $parser->parseFile($file);
+                    $pdf = $parser->parseFile($file1);
                     $signatures =   [];
                     foreach ($pdf->getPages()[0]->getDataTm() as &$value) {
                         if (trim($value[1]) === 'подпись') {
