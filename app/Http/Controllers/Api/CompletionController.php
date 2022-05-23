cbede637fb4e7ece974bb4cba115007a5146d69a
era <ersatama@gmail.com>
2022-05-23 09:59:14 +0600

    fixed completion controller

--- app/Http/Controllers/Api/CompletionController.php
+++ app/Http/Controllers/Api/CompletionController.php
@@ -41,9 +41,9 @@ class CompletionController extends Controller
                 if (Storage::disk('public')->exists($completion->{MainContract::CUSTOMER_ID}.'/completions/'.$completion->{MainContract::ID}.'.pdf')) {
                     $arr[]  =   env('APP_URL','https://admin.car-city.kz').'/storage/'.$completion->{MainContract::CUSTOMER_ID}.'/completions/'.$completion->{MainContract::ID}.'.pdf';
                 } elseif (Storage::disk('public')->exists($completion->{MainContract::CUSTOMER_ID}.'/applications/'.$completion->{MainContract::ID}.'/'.$completion->{MainContract::ID}.'.pdf')) {
-                    $arr[]  =   env('APP_URL','https://admin.car-city.kz').'/storage/'.$completion->{MainContract::CUSTOMER_ID}.'/applications/'.$completion->{MainContract::ID}.'/'.$completion->{MainContract::ID}.'.pdf';
+                    $arr[]  =   env('APP_URL','https://admin.car-city.kz').'/storage/'.$completion->{MainContract::CUSTOMER_ID}.'/completions/'.$completion->{MainContract::ID}.'/'.$completion->{MainContract::ID}.'.pdf';
                 } elseif (Storage::disk('public')->exists($completion->{MainContract::CUSTOMER_ID}.'/applications/'.$completion->{MainContract::ID}.'.zip')) {
-                    $arr[]  =   env('APP_URL','https://admin.car-city.kz').'/storage/'.$completion->{MainContract::CUSTOMER_ID}.'/applications/'.$completion->{MainContract::ID}.'.zip';
+                    $arr[]  =   env('APP_URL','https://admin.car-city.kz').'/storage/'.$completion->{MainContract::CUSTOMER_ID}.'/completions/'.$completion->{MainContract::ID}.'.zip';
                 }
             }
             if (sizeof($arr) > 0) {
