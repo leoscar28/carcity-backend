fe194c79d7a6da3eab66465a44202daddead6494
leoscar28 <leoscar28@gmail.com>
2022-07-22 10:11:01 +0600

    Update UserBannerResource.php

--- app/Http/Resources/UserBanner/UserBannerResource.php
+++ app/Http/Resources/UserBanner/UserBannerResource.php
@@ -37,7 +37,7 @@ class UserBannerResource extends JsonResource
             MainContract::PHONE_VIEW_COUNT => $this->{MainContract::PHONE_VIEW_COUNT},
             MainContract::CREATED_AT =>  date('d M Y H:m',strtotime($this->{MainContract::CREATED_AT})),
             MainContract::UPDATED_AT =>  date('d M Y H:m',strtotime($this->{MainContract::UPDATED_AT})),
-            MainContract::PUBLISHED_AT =>  date('d M Y H:m',strtotime($this->{MainContract::PUBLISHED_AT})),
+            MainContract::PUBLISHED_AT => $this->{MainContract::PUBLISHED_AT} ?  date('d M Y H:m',strtotime($this->{MainContract::PUBLISHED_AT})) : null,
             MainContract::UP =>  $this->{MainContract::UP},
         ];
     }
