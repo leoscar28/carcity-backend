9373d1199976ef17fa7a35544928b84f4f8a6972
leoscar28 <67303134+leoscar28@users.noreply.github.com>
2022-08-05 09:29:06 +0600

    Update UserBannerCreateRequest.php

--- app/Http/Requests/UserBanner/UserBannerCreateRequest.php
+++ app/Http/Requests/UserBanner/UserBannerCreateRequest.php
@@ -41,7 +41,7 @@ class UserBannerCreateRequest extends FormRequest
             MainContract::EMPLOYEE_PHONE =>  'required',
             MainContract::EMPLOYEE_NAME_ADDITIONAL =>  'nullable',
             MainContract::EMPLOYEE_PHONE_ADDITIONAL =>  'nullable',
-            MainContract::IMAGES.'.*' => 'required|image|mimes:jpg,png'
+            MainContract::IMAGES.'.*' => 'required|image|mimes:jpg,jpeg,png,bmp'
         ];
     }
 
