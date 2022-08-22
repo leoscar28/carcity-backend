53fd05d16e745f2727747733fb6c41b23d3e645d
leoscar28 <67303134+leoscar28@users.noreply.github.com>
2022-08-22 12:04:49 +0600

    Update ApplicationRepositoryEloquent.php

--- app/Domain/Repositories/Application/ApplicationRepositoryEloquent.php
+++ app/Domain/Repositories/Application/ApplicationRepositoryEloquent.php
@@ -142,7 +142,7 @@ class ApplicationRepositoryEloquent implements ApplicationRepositoryInterface
             [MainContract::RID,$rid],
             [MainContract::UPLOAD_STATUS_ID,$uploadStatusId],
             [MainContract::STATUS,1]
-        ])->limit(50)->get();
+        ])->limit(100)->get();
     }
 
     public function getByIds($ids)
