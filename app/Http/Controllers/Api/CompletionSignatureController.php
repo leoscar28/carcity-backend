e796cc0ada0eb25970f4f7feace286979d2fd6c4
leoscar28 <67303134+leoscar28@users.noreply.github.com>
2022-08-22 15:38:23 +0600

    Update CompletionSignatureController.php

--- app/Http/Controllers/Api/CompletionSignatureController.php
+++ app/Http/Controllers/Api/CompletionSignatureController.php
@@ -54,6 +54,9 @@ class CompletionSignatureController extends Controller
 
     public function multipleStart($rid,$userId): Response|Application|ResponseFactory
     {
+        ini_set('memory_limit', '-1');
+        ini_set('max_execution_time', '-1');
+        
         $completions    =   $this->completionService->getByRidAndUploadStatusId($rid,1);
         if (sizeof($completions) > 0) {
             $arr    =   [];
